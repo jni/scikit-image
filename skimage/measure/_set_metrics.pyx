31732658de2a57d96390c07b0c9799b767ee9ec6
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-04-08 13:42:43 +1000

    Optimise to prevent slicing in loop

--- skimage/measure/_set_metrics.pyx
+++ skimage/measure/_set_metrics.pyx
@@ -9,15 +9,16 @@ from libc.math cimport sqrt
 import numpy as np
 
 
-MAX_FLOAT64 = np.finfo(np.float64).max
+cdef double MAX_FLOAT64 = np.finfo(np.float64).max
 
 
-cdef double sqeuclidean_dist(double [::1] point0, double[::1] point1):
+cdef double sqeuclidean_dist(double[:, ::1] points0, Py_ssize_t pt0,
+                             double[:, ::1] points1, Py_ssize_t pt1):
     """Compute the squared euclidean distance between two points.
 
     Parameters
     ----------
-    point0, point1 : 1D arrays of double, same size
+    points0, points1 : 2D arrays of double, same size
         The two points for which to compare the distance.
 
     Returns
@@ -25,10 +26,10 @@ cdef double sqeuclidean_dist(double [::1] point0, double[::1] point1):
     dist : double
         The squared euclidean distance between point0 and point1.
     """
-    cdef Py_ssize_t i, dim = point0.shape[0]
+    cdef Py_ssize_t i, dim = points0.shape[1]
     cdef double diff, dist = 0.
     for i in range(dim):
-        diff = point0[i] - point1[i]
+        diff = points0[pt0, i] - points1[pt1, i]
         dist += diff * diff
     return dist
 
@@ -53,7 +54,7 @@ def hausdorff_distance_onesided(double[:, ::1] points_src,
     for i_src in range(points_src.shape[0]):
         currmin = MAX_FLOAT64
         for i_dst in range(points_dst.shape[0]):
-            d = sqeuclidean_dist(points_src[i_src], points_dst[i_dst])
+            d = sqeuclidean_dist(points_src, i_src, points_dst, i_dst)
             currmin = dmin(currmin, d)
             if currmin < maxmin:
                 break  # trip from current point already shorter than curr. max
