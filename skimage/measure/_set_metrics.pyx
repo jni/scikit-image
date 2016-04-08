eb3a20ff9cc24cf0ff235e06374d5fd0477b43d5
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-04-08 14:13:11 +1000

    Docstring fixes

--- skimage/measure/_set_metrics.pyx
+++ skimage/measure/_set_metrics.pyx
@@ -18,8 +18,14 @@ cdef double sqeuclidean_dist(double[:, ::1] points0, Py_ssize_t pt0,
 
     Parameters
     ----------
-    points0, points1 : 2D arrays of double, same size
-        The two points for which to compare the distance.
+    points0 : 2D array of double, shape (M, K)
+        The array containing the source point.
+    pt0 : int
+        The index of the source point within ``points0``.
+    points1 : 2D array of double, shape (N, K)
+        The array containing the destination point.
+    pt1 : int
+        The index of the destination point within ``points1``.
 
     Returns
     -------
@@ -40,12 +46,32 @@ cdef inline double dmin(double x, double y):
 cdef inline double dmax(double x, double y):
     return x if x > y else y
 
+
 def hausdorff_distance_onesided(double[:, ::1] points_src,
                                 double[:, ::1] points_dst):
-    """Compute the one-sided Haussdorff distance between two sets of points.
+    """Compute the one-sided Hausdorff distance between two sets of points.
 
     This is defined as the longest trip that one can make from a point
     in `points_src` to its nearest point in `points_dst`.
+
+    An equivalent expression using the function ``distance_matrix`` from
+    ``scipy.spatial`` would be:
+
+        ``np.max(np.min(distance_matrix(points_src, points_dst), axis=1))``
+
+    This function, however, contains several performance improvements
+    over that solution.
+
+    Parameters
+    ----------
+    points_src, points_dst : 2D arrays of double, shape (M, K) and (N, K)
+        Two arrays of spatial coordinates in K dimensions.
+
+    Returns
+    -------
+    dist : double
+        The one-sided Hausdorff distance between points in ``points_src``
+        and points in ``points_dst``.
     """
     cdef double maxmin = 0.
     cdef double currmin = MAX_FLOAT64, d = 0.
