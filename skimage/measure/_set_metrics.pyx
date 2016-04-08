f48b7a864e5571e626eb1f0f9bc57a93d6d1cf1e
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-04-08 13:29:47 +1000

    Refactor hausdorff_onesided for clarity

--- skimage/measure/_set_metrics.pyx
+++ skimage/measure/_set_metrics.pyx
@@ -12,25 +12,50 @@ import numpy as np
 MAX_FLOAT64 = np.finfo(np.float64).max
 
 
-def hausdorff_distance_onesided(cnp.float64_t[:, ::1] points_sup,
-                                cnp.float64_t[:, ::1] points_inf):
+cdef double sqeuclidean_dist(double [::1] point0, double[::1] point1):
+    """Compute the squared euclidean distance between two points.
+
+    Parameters
+    ----------
+    point0, point1 : 1D arrays of double, same size
+        The two points for which to compare the distance.
+
+    Returns
+    -------
+    dist : double
+        The squared euclidean distance between point0 and point1.
     """
-    Compute the one-sided Haussdorff distance between two sets of points.
+    cdef Py_ssize_t i, dim = point0.shape[0]
+    cdef double diff, dist = 0.
+    for i in range(dim):
+        diff = point0[i] - point1[i]
+        dist += diff * diff
+    return dist
+
+
+cdef inline double dmin(double x, double y):
+    return x if x < y else y
+
+cdef inline double dmax(double x, double y):
+    return x if x > y else y
+
+def hausdorff_distance_onesided(double[:, ::1] points_src,
+                                double[:, ::1] points_dst):
+    """Compute the one-sided Haussdorff distance between two sets of points.
+
+    This is defined as the longest trip that one can make from a point
+    in `points_src` to its nearest point in `points_dst`.
     """
-    assert points_sup.shape[1] == points_inf.shape[1]
+    cdef double maxmin = 0.
+    cdef double currmin = MAX_FLOAT64, d = 0.
+    cdef Py_ssize_t i_src, i_dst
 
-    cdef double d2_max = 0.
-    cdef double d2_min_i = MAX_FLOAT64
-    cdef double d2_j = 0.
-    cdef Py_ssize_t i, j, k
-
-    for i in range(points_sup.shape[0]):
-        d2_min_i = MAX_FLOAT64
-        for j in range(points_inf.shape[0]):
-            d2_j = 0.
-            for k in range(points_sup.shape[1]):
-                d2_j += (points_sup[i, k] - points_inf[j, k])**2
-            d2_min_i = d2_min_i if d2_min_i < d2_j else d2_j
-        d2_max = d2_max if d2_max > d2_min_i else d2_min_i
-
-    return sqrt(d2_max)
+    for i_src in range(points_src.shape[0]):
+        currmin = MAX_FLOAT64
+        for i_dst in range(points_dst.shape[0]):
+            d = sqeuclidean_dist(points_src[i_src], points_dst[i_dst])
+            currmin = dmin(currmin, d)
+            if currmin < maxmin:
+                break  # trip from current point already shorter than curr. max
+        maxmin = dmax(maxmin, currmin)
+    return sqrt(maxmin)
